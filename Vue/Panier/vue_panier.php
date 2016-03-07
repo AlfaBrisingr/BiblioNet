ccfbe5b4f3827cfdc1bb87e3523dceabfe8ec314
AlfaBrisingr <oceane.martin53960@gmail.com>
2016-03-07 23:17:47 +0100

    Bootstrap

--- Vue/Panier/vue_panier.php
+++ Vue/Panier/vue_panier.php
@@ -4,37 +4,48 @@ if(isset($_SESSION['Panier'])){
 	$total = 0;
 	?>
 	<p align="center">Votre panier</p> <br/>
-	<div class="Tableau">
-		<table>
-			<TR> 
-				<TD> Référence </TD>
-				<TD> Désignation </TD>
-				<TD> Prix </TD>
-				<TD> Quantite </TD>
-				<TD> Montant </TD>
-				<TD> Actions </TD> 
-			</TR>
-			<?php
-			$coll = $_SESSION['Panier']->getProduitsPanier();	
-			foreach ($coll as $key => $value) { ?>
+	<div class="row">
+		<div class="clear col-xs-12 col-sm-12">
+			<div class="table-responsive">
+				<div class="overflow-scroll-table-lg">
+					<table class="table table-hover table-condensed table-bordered table-striped">
+						<TR>
+							<TD> Référence </TD>
+							<TD> Désignation </TD>
+							<TD> Prix </TD>
+							<TD> Quantite </TD>
+							<TD> Montant </TD>
+							<TD> Actions </TD>
+						</TR>
+						<?php
+						$coll = $_SESSION['Panier']->getProduitsPanier();
+						foreach ($coll as $key => $value) { ?>
 
-			<TR> 
-				<TD headers="NumLivre"><?php echo $value -> getRef(); ?></TD>
-				<TD><?php echo $value -> getLib(); ?></TD>
-				<TD><?php echo $value -> getPrix()." euros"; ?></TD>
-				<TD headers="quantite"><a href='index.php?uc=Panier&action=diminuerProduit&ref=<?= $value->getRef(); ?>'><img src='img/Panier/EnleverMoins.png'/></a>  <?php echo $value -> getQte(); ?>  <a href='index.php?uc=Panier&action=augmenterProduit&ref=<?= $value->getRef(); ?>'><img src='img/Panier/AjouterPlus.png'/></a></TD>
-				<TD><?php echo $value -> getPrix()*$value ->getQte()." euros"; ?></TD>
-				<TD><a href='index.php?uc=Panier&action=supprimerProduit&ref=<?= $value->getRef(); ?>'><img src='img/Divers/poubelle.png'/></a><br/></TD>
-				<?php $total = $total + $value -> getPrix()*$value ->getQte(); ?>
-			</TR>
-			<?php
-		} ?>
-	</table>
-</div> </br>
+							<TR>
+								<TD headers="NumLivre"><?php echo $value -> getRef(); ?></TD>
+								<TD><?php echo $value -> getLib(); ?></TD>
+								<TD><?php echo $value -> getPrix()." euros"; ?></TD>
+								<TD headers="quantite"><a href='index.php?uc=Panier&action=diminuerProduit&ref=<?= $value->getRef(); ?>'><img src='img/Panier/EnleverMoins.png'/></a>  <?php echo $value -> getQte(); ?>  <a href='index.php?uc=Panier&action=augmenterProduit&ref=<?= $value->getRef(); ?>'><img src='img/Panier/AjouterPlus.png'/></a></TD>
+								<TD><?php echo $value -> getPrix()*$value ->getQte()." euros"; ?></TD>
+								<TD><a href='index.php?uc=Panier&action=supprimerProduit&ref=<?= $value->getRef(); ?>'><img src='img/Divers/poubelle.png'/></a><br/></TD>
+								<?php $total = $total + $value -> getPrix()*$value ->getQte(); ?>
+							</TR>
+							<?php
+						} ?>
+					</table>
+				</div>
 
-<?php echo "Total du prix à payer : ".$total. " euros"; ?></br></br>
-<a href="?uc=Panier&action=validerCommande" class="boutonEC"> Valider la Commande </a>
-<?php
+			</div>
+		</div>
+	</div>
+	</br>
+
+	<div class="row">
+		<?php echo "Total du prix à payer : ".$total. " euros"; ?></br></br>
+		<a href="?uc=Panier&action=validerCommande" class="btn btn-primary"> Valider la Commande </a>
+		<a href="?uc=index" class="btn btn-default">Retour</a>
+	</div>
+	<?php
 }else{
 
 	echo "Votre panier est vide";
--- index.php
+++ index.php
@@ -14,80 +14,88 @@ session_start(); ?>
 <head>
 	<title>BiblioNet</title>
 	<meta http-equiv="Content-Type" content="text/html"; charset="UTF-8" />
-	<link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css' rel='stylesheet'>
+	<link rel="stylesheet" href="style/bootstrap.css" media="screen">
+	<link rel="stylesheet" href="style/usebootstrap.css">
+	<link type='text/css' rel='stylesheet' href='style/main.css'/>
+	<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/usebootstrap.js"></script>
+	<!--<link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css' rel='stylesheet'> -->
 </head>
 
-<body>
 
+<body class="background">
+<!-- Le menu -->
+<div class="navbar navbar-default navbar-fixed-top">
+	<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="?uc=Accueil">BiblioNet</a>
+		</div>
 
-	<body>
-		<!-- Le menu -->
-		<nav class="navbar navbar-default" role="navigation">
-			<!-- Brand and toggle get grouped for better mobile display -->
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="?uc=Accueil">BiblioNet</a>
-			</div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="?uc=Accueil">Accueil</a></li>
+				<li><a href="?uc=Livre">Livre</a></li>
+				<li><a href="?uc=Panier">Panier</a></li>
 
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse navbar-ex1-collapse">
-				<ul class="nav navbar-nav">
-					<li class="active"><a href="?uc=Accueil">Accueil</a></li>
-					<li><a href="?uc=Livre">Livre</a></li>
-					<li><a href="?uc=Panier">Panier</a></li>
-
+			</ul>
+			<?php
+			if(isset($_SESSION['mail'])){
+				?>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="?uc=MonCompte">Mon Compte</a></li>
+					<li><a href="?uc=Deconnexion">Déconnexion</a></li>
 				</ul>
 				<?php
-				if(isset($_SESSION['mail'])){
-					?>
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="?uc=MonCompte">Mon Compte</a></li>
-						<li><a href="?uc=Deconnexion">Déconnexion</a></li>
-					</ul>
-					<?php
-				}else{
-					?>
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="?uc=Connexion">Connexion</a></li>
-						<li><a href="?uc=Inscription">Inscription</a></li>
-					</ul>
-					<?php
-				}
+			}else{
 				?>
-			</div><!-- /.navbar-collapse -->
-		</nav>
-		<div class="container-fluid">
-			<!-- Le corps -->
-			<?php if (isset($_GET['uc']))
-			{
-				switch ($_GET['uc'])
-				{
-					case 'Accueil' : include("Vue/Accueil/vue_accueil.php"); break;
-					case 'Panier' : include("Controleur/Panier/controleur_panier.php"); break;
-					case 'MonCompte' : include("Controleur/Espace Compte/controleur_espacecompte.php"); break;
-					case 'Connexion' : include("Controleur/Connexion/controleur_connexion.php"); break;
-					case 'Inscription' :  include("Controleur/Inscription/controleur_inscription.php"); break;
-					case 'Livre' :  include("Controleur/Livre/controleur_livre.php"); break;
-					case 'Deconnexion' :  include("Controleur/Deconnexion.php"); break;
-					case 'Commentaire' : include("Controleur/Commentaire/controleur_commentaire.php"); break;
-					default : include("Vue/Accueil/vue_accueil.php"); break;
-				}
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="?uc=Connexion">Connexion</a></li>
+					<li><a href="?uc=Inscription">Inscription</a></li>
+				</ul>
+				<?php
 			}
-			else
-			{
-				header('Location: index.php?uc=accueil');
-			}
-
 			?>
-		</div>
-	</body>
+		</div><!-- /.navbar-collapse -->
+	</div>
+</div>
+<div class="container">
+	<!-- Le corps -->
+	<?php if (isset($_GET['uc']))
+	{
+		switch ($_GET['uc'])
+		{
+			case 'Accueil' : include("Vue/Accueil/vue_accueil.php"); break;
+			case 'Panier' : include("Controleur/Panier/controleur_panier.php"); break;
+			case 'MonCompte' : include("Controleur/Espace Compte/controleur_espacecompte.php"); break;
+			case 'Connexion' : include("Controleur/Connexion/controleur_connexion.php"); break;
+			case 'Inscription' :  include("Controleur/Inscription/controleur_inscription.php"); break;
+			case 'Livre' :  include("Controleur/Livre/controleur_livre.php"); break;
+			case 'Deconnexion' :  include("Controleur/Deconnexion.php"); break;
+			case 'Commentaire' : include("Controleur/Commentaire/controleur_commentaire.php"); break;
+			default : include("Vue/Accueil/vue_accueil.php"); break;
+		}
+	}
+	else
+	{
+		header('Location: index.php?uc=accueil');
+	}
 
-	<!--<footer id="pied_de_page">
-        <p> Copyright by BiblioNet, tous droits réservés </p>
-    </footer>-->
+	?>
+</div>
+
+
+</body>
+
+<!--<footer id="pied_de_page">
+    <p> Copyright by BiblioNet, tous droits réservés </p>
+</footer>-->
 </html>
